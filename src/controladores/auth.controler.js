7d76b43505e227702962adf06c177d21e982b87e
elmozapate <85320923+elmozapate@users.noreply.github.com>
2023-11-06 09:08:43 -0500

    Update auth.controler.js

--- src/controladores/auth.controler.js
+++ src/controladores/auth.controler.js
@@ -63,8 +63,9 @@ router.put('/crear',(req,res)=>{
                   })
         )
        }
-      if ( (!req.body.data|| ( !req.body.data=== true ) || typeof req.body.data !== "object") || !modelValidator(req.body).validate){
-      return(   res.status(401).send(
+      if (!req.body.data ||  typeof req.body.data !== "object" ||(  ( !req.body.data=== true ) ) || !modelValidator(req.body).validate){
+             console.log( typeof req.body.data, " type")
+        return(   res.status(401).send(
                   {
                       mensaje:req.body.data? "falta usuario y contraseña":modelValidator(req.body).msg
                   })
