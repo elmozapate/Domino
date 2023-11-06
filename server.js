c5caed6b048c3111a2556f4b190dd7ca48122b2b
elmozapate <85320923+elmozapate@users.noreply.github.com>
2023-11-05 23:08:20 -0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const http = require("http").Server(app)
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const { json } = require('body-parser');
+
+
+const Router=require('./src/router/Router.js')
+const PORT = process.env.PORT || 3001;
+app.use(cors());
+Router(http)
+
+http.listen(PORT, () => {
+  console.log("Servidor activo, escuchando en el puerto ",PORT)
+    
+})
