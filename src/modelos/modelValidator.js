9cc3156c8eb1b07efa53cf391c4fcf558e0c6e75
elmozapate <85320923+elmozapate@users.noreply.github.com>
2023-11-06 07:33:38 -0500

    Update modelValidator.js

--- src/modelos/modelValidator.js
+++ src/modelos/modelValidator.js
@@ -1,1 +1,20 @@
 
+const modelValidator=()=>{
+    const login=(data)=>{
+      console.log("modelv",data)
+        let res={
+            msg:"",
+            validate:true
+            }
+            if (!data.usuario&&res ||!data.password&&res ) {
+                 !data.usuario&&res = res.msg + "falta usuario"
+        !data.password&&res = res.msg + "falta password"
+        res.validate=false
+            }
+
+        return res
+
+    }
+
+}
+module.exports=modelValidator
