0357fca9f5e3647fa62e50fa3c77136b64f7c65e
elmozapate <85320923+elmozapate@users.noreply.github.com>
2023-11-06 00:00:44 -0500

    Create user.servicios.js

---
+++ src/servicios/user.servicios.js
@@ -0,0 +1,28 @@
+class ServiciosUsuarios {
+    static _instanciaEstaticaUsuarios = null
+     constructor(){}
+     static obtenerInstancia() {
+         
+         if (!ServiciosUsuarios._instanciaEstaticaUsuarios) {
+             ServiciosUsuarios._instanciaEstaticaUsuarios = new ServiciosUsuarios()
+         }
+         return (
+             ServiciosUsuarios._instanciaEstaticaUsuarios
+             )
+     }
+     agregar( task,data,token) {
+         switch (task) {
+             case 'test':
+                 console.log(data)
+                 return true
+                 break;
+             
+             default:
+             return false
+                 break;
+                 
+         }
+     }
+     
+}
+module.exports=ServiciosUsuarios
