cc3f44a479e85371e66b816441b794f39cfbc9ec
elmozapate <85320923+elmozapate@users.noreply.github.com>
2023-11-06 02:02:28 -0500

    Update Router.js

--- src/router/Router.js
+++ src/router/Router.js
@@ -3,8 +3,8 @@ const enrutadorUsers=require('../controladores/user.controler.js')
 const Router=(server)=>{
   console.log( " entro a router ")
   server.use('/test',(req,res)=>{
-    console.log(" test",req)
-                  res.status(400).send(
+    console.log(" test",req.body)
+                  res.status(201).send(
                   {
                       mensaje:'cuerpo vacio'
                   }
